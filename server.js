5bebcdcf63abc371afc0eef7c0165900d8c9a209
khatheejareemus95-wq <khatheeja.reemu.s95@kalvium.community>
2025-09-18 09:54:29 +0530

    corrcted the api connection

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());             // enable CORS for frontend
 app.use(express.json());     // parse JSON bodies
-app.use(express.static('index.html'));     // parse JSON bodies
+app.use(express.static('public'));     // parse JSON bodies
 
 app.use('/api/events', eventsRouter);
 
